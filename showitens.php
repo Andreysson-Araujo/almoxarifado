1d0ea0a42211dcc6ca420fd4c8d7602bdf6a47ad
Andreysson-Araujo <andreyssonaraujo@gmail.com>
2024-03-13 14:26:38 -0400

    melhorando tabelas

--- showitens.php
+++ showitens.php
@@ -6,42 +6,44 @@ require_once("models/Message.php");
 
 require_once("globals.php");
 
-
 $itemDao = new ItemDAO($conn, $BASE_URL);
-
 $items = $itemDao->findAll();
 
 include_once("templates/header.php");
 ?>
 
 <div class="container-category">
-    <h1 class="main-title">Resgistro de entrada e saida de itens</h1>
-</br>
+    <h1 class="main-title">Registro de entrada e saída de itens</h1>
     <div class="table-responsive">
         <table class="table">
             <thead>
                 <tr>
                     <th>Item</th>
-                    <th>Patrimonio</th>
+                    <th>Patrimônio</th>
+                    <th>Data de Registro</th>
+                    <th>Registro de</th>
                     <th>Ações</th>
                 </tr>
             </thead>
             <tbody>
                 <?php foreach ($items as $item) : ?>
                     <tr>
-                    <td><?= $item->name ?></td>
-                    <td><?= $item->patrimony ?></td>
-                    <td>
-                        <a href="editcategory.php?id=<?= $category->id ?>" class="btn btn-primary">Editar</a>
-                        <!-- Formulário para exclusão -->
-                        <form action="<?= $BASE_URL ?>showcategories.php" method="POST" style="display: inline;">
-                            <input type="hidden" name="delete_category_id" value="<?= $category->id ?>">
-                            <button type="submit" class="btn btn-danger">Excluir</button>
-                        </form>
-                    </td>
-                    <a href=""></a>
+                        <td><?= $item->name ?></td>
+                        <td><?= $item->patrimony ?></td>
+                        <td><?= $item->public_date ?></td>
+                        <td class="<?= ($item->register_as === 'Entrada') ? 'text-success' : 'text-danger' ?>">
+                            <?= $item->register_as ?>
+                        </td>
+                        <td>
+                            <a href="edititem.php?id=<?= $item->id ?>" class="btn btn-primary">Editar</a>
+                            <!-- Formulário para exclusão -->
+                            <form action="<?= $BASE_URL ?>showitens.php" method="POST" style="display: inline;">
+                                <input type="hidden" name="delete_category_id" value="<?= $item->id ?>">
+                                <button type="submit" class="btn btn-danger">Excluir</button>
+                            </form>
+                        </td>
                     </tr>
-                 <?php endforeach; ?>
+                <?php endforeach; ?>
             </tbody>
         </table>
         <a href="addItem.php" id="btnbtn" class="btn btn-success">Registrar novo item</a>
